293bf96f094f540d86e8e64f0e99fb2fcad4d658
Casie Ropski <casieropski@gmail.com>
2015-11-18 09:20:14 -0500

    Commit 3 - w/ errors
    My code for the slideshow has been glitching - it is meant to have text
    over each image but when I try to add the text, instead it throws the
    image off (image is cut off by black space above/below). I think I
    located the errors in the CSS and I am still troubleshooting.

--- PickAFlickHome.php
+++ PickAFlickHome.php
@@ -42,29 +42,34 @@
         			 <div id="mask">  <!-- Mask -->
 
          			<ul>
-         			<li id="first" class="firstanimation">  <!-- ID for tooltip and class for animation -->
-         			<a href="#"> <img height=350px; width=230px; src="		http://www.impawards.com/1997/posters/george_of_the_jungle_ver3.jpg" alt="George"/> </a>
-         			<div class="tooltip"> <h4> George of the Jungle </h4></div>
-         			</li>
+                    
+        <li id="first" class="firstanimation">  <!-- ID for tooltip and class for animation -->
+        <div class="tooltip"> <h4> #1 </h4></div>
+         <a href="#"> <img height=360px; width=230px; src="		http://www.impawards.com/1997/posters/george_of_the_jungle_ver3.jpg" alt="George"/> </a>
+
+         </li>
 
          <li id="second" class="secondanimation">
-         <a href="#"> <img height=350px; width=230px;  src="http://ia.media-imdb.com/images/M/MV5BMjM5Mzg5NjA0Nl5BMl5BanBnXkFtZTcwMjg3MDEyMw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg" alt="Furry"/> </a>
-         <div class="tooltip"> <h4> Furry Vengeance</h4> </div>
+         <div class="tooltip"> <h4> #2 </h4> </div>
+         <a href="#"> <img height=360px; width=230px;  src="http://ia.media-imdb.com/images/M/MV5BMjM5Mzg5NjA0Nl5BMl5BanBnXkFtZTcwMjg3MDEyMw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg" alt="Furry"/> </a>
          </li>
 
          <li id="third" class="thirdanimation">
-         <a href="#"> <img height=350px; width=230px; src="http://ia.media-imdb.com/images/M/MV5BMTY1OTgyNTI2OV5BMl5BanBnXkFtZTcwNDUxMDQyMQ@@._V1_SY317_CR4,0,214,317_AL_.jpg" alt="breathing"/> </a>
-         <div class="tooltip"> <h4> Still Breathing </h4></div>
+         <div class="tooltip"> <h4> #3 </h4></div>
+         <a href="#"> <img height=360px; width=230px; src="http://ia.media-imdb.com/images/M/MV5BMTY1OTgyNTI2OV5BMl5BanBnXkFtZTcwNDUxMDQyMQ@@._V1_SY317_CR4,0,214,317_AL_.jpg" alt="breathing"/> </a>
+
          </li>
 
          <li id="fourth" class="fourthanimation">
-         <a href="#"> <img height=350px; width=230px; src="http://ecx.images-amazon.com/images/I/51BXhPusZIL._SY355_.jpg" alt="Man"/> </a>
-         <div class="description"> Encind Man </div>
+         <div class="description"> #4 </div>
+         <a href="#"> <img height=360px; width=230px; src="http://ecx.images-amazon.com/images/I/51BXhPusZIL._SY355_.jpg" alt="Man"/> </a>
+
          </li>
 
          <li id="fifth" class="fifthanimation">
-         <a href="#"> <img height=350px; width=230px;  src="http://i43.tower.com/images/mm107062438/monkeybone-brendan-fraser-dvd-cover-art.jpg" alt="Monkey"/> </a>
-         <div class="tooltip"> Monkey Bone </div>
+         <div class="tooltip"> #5 </div>
+         <a href="#"> <img height=360px; width=230px;  src="http://i43.tower.com/images/mm107062438/monkeybone-brendan-fraser-dvd-cover-art.jpg" alt="Monkey"/> </a>
+
          </li>
          
          </ul>
