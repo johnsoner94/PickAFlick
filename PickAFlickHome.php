f18c29669c3edaf25c05d51ce9b651ad9cc39012
johnsoner94 <johnsoner94@gmail.com>
2015-12-09 16:55:28 -0500

    Corrected Posters
    I think this is what Casie asked for. Aliya, you don’t have to do it
    now.

--- PickAFlickHome.php
+++ PickAFlickHome.php
@@ -6,74 +6,6 @@
 <link href="PickAFlick.css" rel="stylesheet" type="text/css">
 </head>
  
-<body>
-  <header>
-	<h1 class='retroshadow'> Pick A Flick </h1> 
-        <div class="nav">
-      	<ul>
-        	<li class="home"><a href="PickAFlickHome.php">Home</a></li>
-        	<li class="profile"><a href="PickAFlickProfile.php">My Profile</a></li>
-            <li class="pick"><a href="PickAFlickPick.php">Pick</a></li>
-        	<li class="help"><a href="#">Help</a></li>
-			<li>
-				<form action="" method="post">
-                    <input type="text" name="term" placeholder="Search by title..." required>
-                    <input type="submit" value="Search">
-				</form> 
-			</li>
-		</ul>
-	</div>
-</header>
-
-	<h2> Welcome to Pick A Flick, a movie generator based on your personal mood and interests. </h2>
-
-	<h3> Make sure to <a href="PickAFlickPick.php">Take Your Pick!</a></h3>
-				
-<?php
-if (!empty($_REQUEST['term'])) {
-
-	$term = mysql_real_escape_string($_REQUEST['term']);  
-	if(strpos($term, "\'\'"))
-	{
-		$term = str_replace("\'", "'", $term);
-		$term = substr($term, ($pos = strpos($term, "''")) !== false ? $pos + 1 : 0);
-		$term = "'" . $term;	
-
-	}
-	elseif($term == "\'\'")
-	{
-		$term = str_replace("\'", "'", $term);
-	}
-	elseif($term == "\'")
-	{
-		$term = str_replace("\'", "'", $term);
-	}
-	elseif(strpos($term, "\'"))
-	{
-		$term = str_replace("\'", "''", $term);
-		$term = substr($term, ($pos = strpos($term, "''")) !== false ? $pos + 1 : 0);
-		$term = "'" . $term;
-	}
-
-	$sql = "SELECT * FROM movtest WHERE title LIKE '%".$term."%'"; 
-	$r_query = mysql_query($sql); 
-
-	while ($row = mysql_fetch_array($r_query)){
-		echo '<br /> Title: ' .$row['title'];  
-		echo '<br /> Year: '.$row['year'];  
-		echo '<br /> Rating: '.$row['rating'];  
-		echo '<br /> Duration: '.$row['duration'];
-		echo '<br /> <img src='.$row['poster'].' alt="moviePoster" width="300" heigth="500">';
-	}  
-
-}
-?>
-  
-  <table style="width:100%">
-	<tr>
-		<td>
-        <h2> Looking to get ready for the Holiday season? Check out these seasonal favorites!  </h2>
-        <img src="http://www.mainelights.org/images/lights03a.gif" alt="Christmas Lights">
         
 		<?php 
 		// checks if query can be executed 
@@ -117,12 +49,6 @@ if (!empty($_REQUEST['term'])) {
 			}
 		}
 		
-
-		$query = "        
-        SELECT tagtest.name, pairingtest.movie_id, movtest.title FROM tagtest, pairingtest, movtest WHERE pairingtest.tag_id = tagtest.id AND name LIKE '%christmas%' AND pairingtest.movie_id = movtest.id";
-		$result = mysql_query($query);
-		error_check($result);
-		runQuery($result);
 		
 		
 		?>
@@ -150,13 +76,15 @@ if (!empty($_REQUEST['term'])) {
   
   <h2> Welcome to Pick A Flick, a movie generator based on your personal mood and interests. </h2>
   
-  <h3> Make sure to <a href="PickAFlickPick.php">Take Your Pick!</h3>
+  <h3> Make sure to <a href="PickAFlickPick.php">Take Your Pick!</a></h3>
   
   <table style="width:100%">
 	<tr>
 		<td>
         <h2> Looking to get ready for the Holiday season? Check out these seasonal favorites!  </h2>
+		<div style="text-align: center">
         <img src="http://www.mainelights.org/images/lights03a.gif" alt="Christmas Lights">
+		</div>
         
         
 		
@@ -166,83 +94,52 @@ if (!empty($_REQUEST['term'])) {
 		error_check($result);
 		$row = mysql_fetch_array($result);
 		?>
-
-
-		<div class="container">
-   			<div id="content-slider">
-      			<div id="slider">  <!-- Slider container -->
-        			 <div id="mask">  <!-- Mask -->
-
-         			<ul>
-            
-		        <li id="first" class="firstanimation">  <!-- ID for tooltip and class for animation -->
-		         <div class="tooltip"> <h4> #1 </h4> </div>
-		         <a href="#"> 
-					 <?php
-					 print "<img src=' $row[0]' alt='George' height=360px width=230px /> ";
-					 $row = mysql_fetch_array($result);
-					 reset($row);
-					 ?>
-				 </a>
-
-		         </li>
-
-		         <li id="second" class="secondanimation">
-		         <div class="tooltip"> <h4> #2 </h4> </div>
-		         <a href="#"> 
-					 <?php
-					 print "<img height=360px width=230px src=' $row[0] ' alt='Furry'/> ";
-					 $row = mysql_fetch_array($result);
-					 reset($row);
-					 ?>
-				 </a>
-		         </li>
-
-		         <li id="third" class="thirdanimation">
-		         <div class="tooltip"> <h4> #3 </h4></div>
-		         <a href="#"> 
-					 <?php
-					 print "<img height=360px width=230px src='$row[0]' alt='breathing'/> ";
-					 $row = mysql_fetch_array($result);
-					 reset($row);
-					 ?>
-				</a>
-
-		         </li>
-
-		         <li id="fourth" class="fourthanimation">
-		         <div class="tooltip"><h4>  #4 </h4></div>
-		         <a href="#">
-					 <?php
-					 print "<img height=360px width=230px src='$row[0]' alt='Man'/> ";
-					 $row = mysql_fetch_array($result);
-					 reset($row);
-					 ?>
-				 </a>
-
-		         </li>
-
-		         <li id="fifth" class="fifthanimation">
-		         <div class="tooltip"><h4>  #5 </h4></div>
-		         <a href="#">
-					 <?php
-					 print "<img height=360px width=230px src='$row[0]' alt='Monkey'/> ";
-					 ?>
-				 </a>
-
-		         </li>
- 
-		         </ul>
- 
-		         </div>  <!-- End Mask -->
-		         <div class="progress-bar"></div>  <!-- Progress Bar -->
-		      </div>  <!-- End Slider Container -->
-		   </div>
-		</div>
 		
-
+		<table align="center">
+			<tr>
+				<td>
+				 <?php
+				 print "<img src=' $row[0]' alt='Christmas1' height=360px width=230px /> ";
+				 $row = mysql_fetch_array($result);
+				 reset($row);
+				 ?>
+				</td>
+				<td>
+				 <?php
+				 print "<img src=' $row[0]' alt='Christmas2' height=360px width=230px /> ";
+				 $row = mysql_fetch_array($result);
+				 reset($row);
+				 ?>
+				</td>
+				<td>
+				 <?php
+				 print "<img src=' $row[0]' alt='Christmas3' height=360px width=230px /> ";
+				 $row = mysql_fetch_array($result);
+				 reset($row);
+				 ?>
+				</td>
+				<td>
+				 <?php
+				 print "<img src=' $row[0]' alt='Christmas4' height=360px width=230px /> ";
+				 $row = mysql_fetch_array($result);
+				 reset($row);
+				 ?>
+				</td>
+			</tr>
+			<tr>
+				<td align="center">
+				
+				</td>
+			</tr>
+		</table>
+		<div style="text-align: center">
+	 <?php
+	 print "<img src=' $row[0]' alt='Christmas5' height=360px width=230px /> ";
+	 $row = mysql_fetch_array($result);
+	 reset($row);
+	 ?>
+ </div>
         </td>
-   
 		<td>
 		<?php
 		$query = "SELECT poster FROM movtest WHERE id BETWEEN 1 and 5;";
@@ -260,7 +157,7 @@ if (!empty($_REQUEST['term'])) {
                     
         <li id="first" class="firstanimation">  <!-- ID for tooltip and class for animation -->
          <div class="tooltip"> <h4> #1 </h4> </div>
-         <a href="#"> 
+         <a href="javascript: return false"> 
 			 <?php
 			 print "<img src=' $row[0]' alt='George' height=360px width=230px /> ";
 			 $row = mysql_fetch_array($result);
@@ -272,7 +169,7 @@ if (!empty($_REQUEST['term'])) {
 
          <li id="second" class="secondanimation">
          <div class="tooltip"> <h4> #2 </h4> </div>
-         <a href="#"> 
+         <a href="javascript: return false"> 
 			 <?php
 			 print "<img height=360px width=230px src=' $row[0] ' alt='Furry'/> ";
 			 $row = mysql_fetch_array($result);
@@ -283,7 +180,7 @@ if (!empty($_REQUEST['term'])) {
 
          <li id="third" class="thirdanimation">
          <div class="tooltip"> <h4> #3 </h4></div>
-         <a href="#"> 
+         <a href="javascript: return false"> 
 			 <?php
 			 print "<img height=360px width=230px src='$row[0]' alt='breathing'/> ";
 			 $row = mysql_fetch_array($result);
@@ -295,7 +192,7 @@ if (!empty($_REQUEST['term'])) {
 
          <li id="fourth" class="fourthanimation">
          <div class="tooltip"><h4>  #4 </h4></div>
-         <a href="#">
+         <a href="javascript: return false">
 			 <?php
 			 print "<img height=360px width=230px src='$row[0]' alt='Man'/> ";
 			 $row = mysql_fetch_array($result);
@@ -307,7 +204,7 @@ if (!empty($_REQUEST['term'])) {
 
          <li id="fifth" class="fifthanimation">
          <div class="tooltip"><h4>  #5 </h4></div>
-         <a href="#">
+         <a href="javascript: return false">
 			 <?php
 			 print "<img height=360px width=230px src='$row[0]' alt='Monkey'/> ";
 			 ?>
